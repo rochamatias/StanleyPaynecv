47d84c3ee733d0415924a42c2c7b32e3e74c079b
matias <mr2975965@gmail.com>
2023-01-26 08:05:50 -0300

    mi primer proyecto

---
+++ script.js
@@ -0,0 +1,9 @@
+var button = document.getElementById("boton");
+button.addEventListener("click", function() {
+  alert("Holaa mi nombre es Stanley Payne, muchas gracias por visitar mi sitio!");
+});
+
+function togglepopup(){
+  document.getElementById("popud-1").classList.toggle("active");
+}
+
